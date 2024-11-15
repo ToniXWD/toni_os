731d3b892a27ff1e570a044e8fe63867a3e96fcc
ToniXWD <807077266@qq.com>
2024-11-16 01:26:33 +0800

    09: 分页实现

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,11 @@ pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
+pub mod memory;
+
 use core::panic::PanicInfo;
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
 
 pub fn init() {
     gdt::init();
@@ -73,10 +77,13 @@ pub fn hlt_loop() -> ! {
     }
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@
 #![test_runner(toni_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use toni_os::println;
+use toni_os::{memory, println};
+use x86_64::{structures::paging::Page, VirtAddr};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+// 为了确保入口点函数总是具有引导程序所期望的正确签名，bootloader板块提供了一个entry_point宏，
+// 它提供了一种类型检查的方法来定义一个Rust函数作为入口点
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     toni_os::init();
@@ -16,14 +21,19 @@ pub extern "C" fn _start() -> ! {
     #[cfg(test)]
     test_main();
 
-    // // 故意触发 page fault
-    // let ptr = 0xdeadbeaf as *mut u8;
-    // unsafe { *ptr = 42; }
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // new: initialize a mapper
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator =
+        unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    // 查看内核中页表的存储方式
-    use x86_64::registers::control::Cr3;
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    // 映射未使用的页
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // 通过新的映射将字符串 `New!`  写到屏幕上。
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     println!("It did not crash!");
     toni_os::hlt_loop();
